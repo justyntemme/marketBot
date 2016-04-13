5c78fa15ed7a5d12702fe3be309395b87d06f5b6
user <user@localhost.localdomain>
2016-04-13 00:35:36 -0500

    First commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct chat_node 
+	{
+	int (*next_functions_addr)[];
+	char *words[15];
+	char user_words[];
+	} chat_node;
+
+typedef struct order_intro
+	{
+		//definition of order_intro... adding structures almost as class objects too be navigated. chat_node will crawl the tree with each branch being 
+		//specified as a structure since instead of being generic,we want the customer to follow a path.
+	
+
+		char *reference_words[15]=NULL;
+		reference_words[0]="apple";
+		reference_words[1]="banana";
+		reference_words[2]="exit";
+
+
+	
+	}  order_intro;
+
+
+
+int main(int argc, char *argv[])
+	{
+
+	return 0;
+	}
+
+
+int *find_next_addr(chat_node  *current_chat_node)
+	{
+		int (*next_addr) = NULL;
+		for (int i = 0; i < (sizeof(current_chat_node->words)/sizeof(current_chat_node->words[0]));i++)
+			{
+			}
+		return next_addr;
+	}
