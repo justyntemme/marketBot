c7e7de5e35eb2bef7678fe8211581315c5a7282c
Justyn Temme <justyntemme@gmail.com>
2016-04-13 00:51:45 -0500

    added tree layout for ease of development

--- main.c
+++ main.c
@@ -7,6 +7,26 @@ typedef struct chat_node
 	char user_words[];
 	} chat_node;
 
+//list of how tree should be structured, more for me than actuall documentation
+//                                                  Intro node (cout suggestion words?)
+//				[]					[]								[]
+//                        {list items}                    {list cart(for each in card price+list)}            {checkout(cout << enter fake cc #)(sanitize etc)}
+//             []           	[]            []
+//   {apples(add to cart)}    {banana(add to cart)}      {milk(add to cart $price of milk)}									
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 typedef struct order_intro
 	{
 		//definition of order_intro... adding structures almost as class objects too be navigated. chat_node will crawl the tree with each branch being 
