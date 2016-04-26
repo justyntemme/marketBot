9fd4fbee65aafe60a0b28b77ecc04dacd0ccfd91
user <user@linux-z5rr.CoxCommunications>
2016-04-26 17:47:45 -0500

    added the first reference words, wootwoot train is a trucking

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 
 /* All Functions ending with a comment are functions from the same project
  * as dlist.h and have only changed small amounts if any. all work on functions
- * are pushed upstream to the dutils project
+ * are offered pushed upstream to the dutils project
  *
  *
  */
+
 struct dlist_node *dlist_node_new(struct dlist_list *list,
-				  int *data, void (*dalloc)(void *))
+				  void *data, void (*dalloc)(void *))
 {
 	struct dlist_node *node = NULL;
 
@@ -17,6 +18,7 @@ struct dlist_node *dlist_node_new(struct dlist_list *list,
 		//FIXME: add support for custom error loggin and msg
 		fprintf(stderr,"%s[%d]:%s alloc failed\n", __FILE__,
 			__LINE__,__func__);
+
 		return NULL;
 	}
 
@@ -24,10 +26,18 @@ struct dlist_node *dlist_node_new(struct dlist_list *list,
 	node->data_dalloc = dalloc;
 	node->next = NULL;
 	node->prev = NULL;
-	list->count++;
 
 	return node;
 }/* dlist_node_new */
+
+struct reference_list
+{
+	char *words[100];
+	int points[100];
+	char *goal[100];
+
+
+};
 
 void dlist_node_foreach(struct dlist_list *list,
 			void *(*action)(void *carry, void *data, void *param),
@@ -45,7 +55,7 @@ void dlist_node_foreach(struct dlist_list *list,
 	*(int *)carry = 0;
 	for(iter = list->head; NULL != iter; iter = iter->next)
 	{
-		carry = action(carry, iter->data, param);
+		carry = action(carry, &iter->data, param);
 
 	}
 	return;
@@ -64,29 +74,50 @@ struct dlist_list *dlist_init(struct dlist_list *list,
 	return list;
 }/* dlist_init */
 
-void *fillString(int *carry, int *data, void *param) //implament point system here. a for loop for the size of array might be a better option, then a case
-						       // switch if the word is one of the reference words. with the last jjcase being nill
+void *fillString(int *carry,struct reference_list *rlist,int *param) //implament point system here. a for loop for the size of array might be a better option, then a case
 {	
+	rlist->words[0] = 'c';		
+	
+	
+	switch (*carry)
+	{
+		case 0:
+			printf("At first node\n");
+			rlist->words[0] = "buy\0";
+			rlist->words[1] = "apple\0";
+			rlist->words[2] = "banana\0";
+			rlist->words[3] = "checkout\0";
+			rlist->words[4] = "price\0";
+			printf("char is %s \n",rlist->words[0]);
+			break;
+		case 1:
+			printf("At 2nd node\n");
+			rlist->words[0] = "123";
+			break;
+		case 3:
+			printf("At 3rd node\n");
+			rlist->words[0] = "c";
+			break;
+	}
 	*(int *)carry+=1;
-	//printf("Count: %d\n",*(int *)carry);
 	// TODO add DATA param with reference words. Fun stuff is about to begain	
 	return (carry);
 }
+
 inline size_t dlist_get_size(struct dlist_list *list)
 {
 	return list->count;
 }/* dlist_get_size */
-
-
 
 void populateLists(struct dlist_list *list, int size) //TODO add data as an argument. the wordlist
 {
 	
 
 	for (int i = 0; i < size; i++)
-	{
-
-		dlist_node_append(list,malloc(sizeof(struct dlist_node)));
+	{	struct reference_list *temp_rlist = malloc(sizeof(struct reference_list));
+		struct reference_node *tempNode = dlist_node_new(list,temp_rlist,NULL);
+		dlist_node_append(list,dlist_node_new(list,tempNode,NULL));
+		printf("%i\n",i);
 		
 	}
 	
@@ -127,8 +158,16 @@ int main(int argc, char *argv[])
 	struct dlist_node *node = master_list->head;
 	//get list size
 	int listSize = dlist_get_size(master_list);
+	struct reference_list *rlist = &(master_list->head->data);
+	
 	
 	dlist_node_foreach(master_list,fillStringPointer,NULL);
+	printf("Address of main rlist is %d\n",&rlist);
+	printf("char is %s \n",rlist->words[0]);
+	rlist = &(master_list->head->next->data);
+	printf("char is %c \n",rlist->words[0]);
+	rlist = &(master_list->head->next->next->data);
+	printf("char is %c \n",rlist->words[0]);
 
 
 	return 0;
